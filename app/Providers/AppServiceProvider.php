7e1f6ef81f7d9439c9341cb92907fc10eedcad84
AhmadSappauni <ahmadsapauni8989@gmail.com>
2026-04-08 22:47:49 +0800

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,22 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot()
     {
         Paginator::useBootstrap();
+
+        // 🔥 TAMBAHKAN INI
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
